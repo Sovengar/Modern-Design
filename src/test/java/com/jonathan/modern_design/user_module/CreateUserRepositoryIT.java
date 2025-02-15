69a2f095bdbf6d9d80a9f9ea14bbca1188a83afa
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-02-15 13:40:48 +0100

    improving test

--- src/test/java/com/jonathan/modern_design/user_module/CreateUserRepositoryIT.java
+++ src/test/java/com/jonathan/modern_design/user_module/CreateUserRepositoryIT.java
@@ -3,13 +3,20 @@ package com.jonathan.modern_design.user_module;
 
 import com.jonathan.modern_design.__config.RepositoryITConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 
 import static com.jonathan.modern_design._fake_data.CreateUserMother.createUserCommandWithValidData;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Import(UserConfiguration.class)
 class CreateUserIT extends RepositoryITConfig {
-    private final UserRepository repository = new InMemoryUserRepository();
-    private final UserFacade userFacade = new UserConfiguration().userFacade(repository);
+
+    @Autowired
+    private UserRepositorySpringAdapter repository;
+
+    @Autowired
+    private UserFacade userFacade;
 
     @Test
     void should_create_user() {
