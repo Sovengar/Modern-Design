7df6dffaa12f12278e61b458eba169f399a5235c
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-02-23 15:40:10 +0100

    add microtype to user

---
+++ src/main/java/com/jonathan/modern_design/user_module/domain/model/User.java
@@ -0,0 +1,21 @@
+package com.jonathan.modern_design.user_module.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+public class User {
+    private UserId id;
+    private UUID uuid;
+    private UserRealName realname;
+    private UserName username;
+    private UserEmail email;
+    private UserPassword password;
+    private String country;
+
+    record UserId(Long id) {
+    }
+}
