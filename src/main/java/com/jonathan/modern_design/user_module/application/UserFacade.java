c51461881e3067364020dae99c104af3baadec42
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-03-15 16:19:19 +0100

    refactor: rename UserFacade and add logging for user registration and retrieval

--- src/main/java/com/jonathan/modern_design/user_module/application/UserFacade.java
+++ src/main/java/com/jonathan/modern_design/user_module/application/UserFacade.java
@@ -1,30 +1,37 @@
-package com.jonathan.modern_design.user_module.infra;
+package com.jonathan.modern_design.user_module.application;
 
 import com.jonathan.modern_design._infra.config.annotations.Inyectable;
 import com.jonathan.modern_design.user_module.UserApi;
-import com.jonathan.modern_design.user_module.application.UserRegister;
 import com.jonathan.modern_design.user_module.domain.User.UserId;
 import com.jonathan.modern_design.user_module.domain.UserRepo;
 import com.jonathan.modern_design.user_module.dtos.UserRegisterCommand;
 import com.jonathan.modern_design.user_module.dtos.UserResource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
 @Inyectable
 @RequiredArgsConstructor
-class UserFacade implements UserApi {
+@Slf4j
+public class UserFacade implements UserApi {
     private final UserRepo userRepo;
     private final UserRegister userRegister;
 
     @Override
     @Transactional
     public void registerUser(UserRegisterCommand command) {
+        log.info("BEGIN RegisterUser");
         userRegister.registerUser(command);
+        log.info("END RegisterUser");
     }
 
     @Override
     public UserResource findUser(UserId userId) {
+        log.info("BEGIN FindUser");
         final var user = userRepo.findByUUIDOrElseThrow(userId);
-        return UserResource.from(user);
+        var userResource = UserResource.from(user);
+
+        log.info("END FindUser");
+        return userResource;
     }
 }
