a2aad41e84f6d8a9c9aea93aadf10f4be3ec62e5
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-04-20 00:08:06 +0200

    a

--- src/main/java/jonathan/modern_design/user/alternatives/Endpoints.java
+++ src/main/java/jonathan/modern_design/user/alternatives/Endpoints.java
@@ -1,4 +1,4 @@
-package jonathan.modern_design.user.infra;
+package jonathan.modern_design.user.alternatives;
 
 //Controller could be moved to facade since at this point is a middle-man antipattern
 //This example shows how to contralize endpoints in a single private file
