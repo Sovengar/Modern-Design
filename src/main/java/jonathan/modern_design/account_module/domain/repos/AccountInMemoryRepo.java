d31b1f09e855b89b025c4603dbf6ba926af528e5
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-04-20 17:09:42 +0200

    a

--- src/main/java/jonathan/modern_design/account_module/domain/repos/AccountInMemoryRepo.java
+++ src/main/java/jonathan/modern_design/account_module/domain/repos/AccountInMemoryRepo.java
@@ -2,15 +2,12 @@ package jonathan.modern_design.account_module.domain.repos;
 
 import jonathan.modern_design._common.annotations.Fake;
 import jonathan.modern_design.account_module.domain.Account;
-import jonathan.modern_design.account_module.domain.AccountEntity;
 import jonathan.modern_design.account_module.domain.vo.AccountAccountNumber;
-import lombok.NonNull;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
 
 @Fake //This class is for unit tests, also, don't evaluate his state, pointless, rather evaluate the state of the objects
@@ -21,12 +18,6 @@ public class AccountInMemoryRepo implements AccountRepo {
     public Optional<Account> findOne(String accountNumber) {
         var account = accounts.get(accountNumber);
         return ofNullable(account);
-    }
-
-    @Override
-    public Optional<AccountEntity> findOneEntity(final @NonNull String accountNumber) {
-        var account = accounts.get(accountNumber);
-        return of(new AccountEntity(account));
     }
 
     @Override
