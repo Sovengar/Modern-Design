1f65c147a0e7cd3e2d3f3b3018041393d4065b91
Sovengar <32403261+Sovengar@users.noreply.github.com>
2025-06-28 02:59:23 +0200

    Add catalog API; implement AddReview and GetProduct endpoints for product reviews and retrieval

--- src/main/resources/db/migrations/V10__auth.sql
+++ src/main/resources/db/migrations/V10__auth.sql
@@ -21,3 +21,14 @@ create TABLE AUTH.USERS (
     deleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (role_code) REFERENCES AUTH.ROLES(role_code)
 );
+
+insert into AUTH.ROLES (role_code, description) values
+('ADM', 'Administrator'),
+('TEC', 'Technician'),
+('USER', 'User');
+
+insert into AUTH.USERS (user_id, username, email, internal_enterprise_email, password, status, role_code, version, created_at, created_by) values
+('f5e4e2a8-7f07-4b2e-9b7c-6c6b1fcd3c42', 'userJD', 'V4u0A@example.com', 'emailInBadState@ggg.com', 'encryptedPassword', 'ACTIVE', 'ADM', 0, '2025-02-05 14:00:00', 'SYSTEM'),
+('56fa9d40-19ee-4c30-9438-33d364bafef9', 'user2', '13123121@example.com', 'abc@gmail.com', 'encryptedPassword', 'ACTIVE', 'TEC', 0, '2025-02-05 14:00:00', 'SYSTEM'),
+('3a263b30-d80d-42d4-b64a-2243d258467f', 'user3', '4345534@example.com', 'emailInBadState@ggg.com', 'encryptedPassword', 'ACTIVE', 'TEC', 0, '2025-02-05 14:00:00', 'SYSTEM');
+
--- src/main/resources/db/migrations/V2025_03_09_15_30__catalogs.sql
+++
@@ -1,4 +0,0 @@
-insert into AUTH.ROLES (role_code, description) values
-('ADM', 'Administrator'),
-('TEC', 'Technician'),
-('USER', 'User');
